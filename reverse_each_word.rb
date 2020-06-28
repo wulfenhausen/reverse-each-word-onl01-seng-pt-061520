d5722d550ea462c9c85ba12fa6de6e7c847cc3c1
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 03:21:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -26,3 +26,11 @@ end
   nuarray.join(" ")
 end
     
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
+end
