8f6c7801f3100a6a7cb705ece70d9f20f113d5cb
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 03:24:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,20 +17,26 @@ end
 #  nuarray.join(" ")
 #end
   
-#  def reverse_each_word_with_collect(string)
-#  array = string.split(" ")
-#  nuarray = []
-#  array.collect do |string|
-##    nuarray << string.reverse
-#  end
-#  nuarray.join(" ")
-#end
-    
-def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+  def reverse_each_word_with_collect(string)
+#def reverse_each_word(string)
+  array = string.split(" ")
+#  array = string.split(" ") #turn string into an array
+  nuarray = []
+#  test_array = []
+  array.collect do |string|
+#  array.collect do|string| #iterate over the array
+    nuarray << string.reverse
+#    test_array << string.reverse #reverse each word in the array
   end
-  test_array.join(" ")
+#  end
+  nuarray.join(" ")
+#  test_array.join(" ")
 end
+ #end   
+
+
+
+
+
+
+
