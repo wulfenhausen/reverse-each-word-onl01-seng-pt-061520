41e443395bf2a138e6fa4d49b28f5f8214ee5c80
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 03:31:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,35 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  nuarray = []
+  array.each do |string|
+    nuarray << string.reverse
+  end
+  nuarray.join(" ")
+end
+  
+  
+#def reverse_each_word_with_collect(string)
+#  array = string.split(" ")
+#  nuarray = []
+#arrauy.collect do |string|
+ #    nuarray << string.reverse
+#  end
+#  nuarray.join(" ")
+#end
+  
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  nuarray = []
+  array.collect do|string|
+  nuarray << string.reverse
+end
+nuarray.join(" ")
+end
+
+
+
+
+
+
+
